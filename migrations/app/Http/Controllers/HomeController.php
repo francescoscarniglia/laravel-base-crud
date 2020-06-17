f3af51bcbe278c1aea7bcc85e494ad0648ef53bb
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-17 23:48:34 +0200

    complete pt 1

--- migrations/app/Http/Controllers/HomeController.php
+++ migrations/app/Http/Controllers/HomeController.php
@@ -9,11 +9,27 @@ class HomeController extends Controller
 {
     public function index(){
         // query - ottengo tutti i record da una tabella
-        $students = Student::all();
-        @dd($students);
+        //$students = Student::all(); //select * from students
+        //@dd($students);
+        
+        // WHERE 
+        // $students = Student::where('id', 2)->get();
+        //  3 params
+        // $students = Student::where('id', '<>', '1')
+        //     ->orderBy('name', 'asc') //due parametri, 1° = colonna / 2° = direzione
+        //     ->limit(2)
+        //     ->get(); 
 
+        // FIRST
+
+        // $students = Student::where('id',  '1')->first();
+        // dd($students);
+        // $students[] = Student::where('id',  '1')->first();
+
+        // FIND
+        $students[] = Student::find(2);
         
         // view - welcome
-        return view('welcome');
+        return view('welcome', compact('students'));
     }
 }
--- migrations/resources/views/welcome.blade.php
+++ migrations/resources/views/welcome.blade.php
@@ -6,6 +6,21 @@
     <title>Eloquent ORM</title>
 </head>
 <body>
+    <header>
     <h2>Eloquent ORM</h2>
+    </header>    
+
+    <main>
+        <h3>Students list</h3>
+
+        @foreach ($students as $student)
+        <div class)"student">
+            <!-- $student diventa un'oggetto (name/description sono proprietà dell'oggetto $student) -->
+            <h4> {{ $student-> name }} </h4>
+            <p> {{ $student-> description }}</p>
+        </div>
+        @endforeach
+    </main>
+    
 </body>
 </html>
