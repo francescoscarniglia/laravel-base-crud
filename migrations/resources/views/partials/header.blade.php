d1962420cb2b51a0671be0bf71175f94bfda5893
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-18 15:27:04 +0200

    continue PT- 2
    add migration students

--- migrations/resources/views/partials/header.blade.php
+++ migrations/resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
     
-    <title> Laravel CRUD parte 1</title>
+    <title> Laravel CRUD parte 2</title>
 </head>
 <body>
     <header>
